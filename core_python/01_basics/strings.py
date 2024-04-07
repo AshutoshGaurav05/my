252a9db7f8545aeeb2b785620365e2881b4e939a
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-04-07 19:26:35 +0530

    string in python

---
+++ core_python/01_basics/strings.py
@@ -0,0 +1,48 @@
+chai = "Masala Chai"
+sec_chai = "  Ginger    Chai       "
+quantity = 2
+first_char = chai[0]
+order = "I would like to order {} cups of {}."
+chai_variety = ["Masala Chai", "Ginger Chai", "Cardamom Chai", "Cinnamon Chai", ]
+print(", ".join(chai_variety))
+
+# print(order.format(quantity, chai))
+
+# print('first_char:', first_char)
+# print('slice_chai : ', chai[0:6])
+
+# print('chai.upper():', chai.upper())
+# print('chai.lower():', chai.lower())
+# print('chai.capitalize():', chai.capitalize())
+# print('chai.title():', chai.title())
+# print('chai.swapcase():', chai.swapcase())
+# print('chai.count("a"):', chai.count("a"))
+# print('chai.count("a", 0, 5):', chai.count("a", 0, 5))
+# print('chai.index("a"):', chai.index("a"))
+# print('chai.index("a", 0, 5):', chai.index("a", 0, 5))
+# print('chai.find("a"):', chai.find("a"))
+# print('chai.find("a", 0, 5):', chai.find("a", 0, 5))
+# print('chai.startswith("Masala"):', chai.startswith("Masala"))
+# print('chai.endswith("Chai"):', chai.endswith("Chai"))
+# print('chai.replace("Masala", "Ginger"):', chai.replace("Masala", "Ginger"))
+# print('sec_chai.strip():', sec_chai.strip())
+# print('sec_chai.lstrip():', sec_chai.lstrip())
+# print('sec_chai.rstrip():', sec_chai.rstrip())
+# print('chai.split():', chai.split(" "))
+# print('chai.partition("a"):', chai.partition("a"))
+# print('chai.rpartition("a"):', chai.rpartition("a"))
+# print('chai.join("a"):', chai.join("a"))
+
+
+
+
+
+num_list = "0123456789"
+# print('num_list:', num_list)
+# print('num_list[0:5]:', num_list[0:5])
+# print('num_list[5:]:', num_list[5:])
+# print('num_list[:5]:', num_list[:5])
+# print('num_list[:7:2]:', num_list[:7:2])    
+# print('num_list[::2]:', num_list[::2])
+# print('num_list[1::2]:', num_list[1::2])
+# print('num_list[::-1]:', num_list[::-1])
