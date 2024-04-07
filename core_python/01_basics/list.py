f12c208066ef9290ea4caf27e2bf5881105fd4d6
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-04-07 19:27:09 +0530

    list in python

---
+++ core_python/01_basics/list.py
@@ -0,0 +1,27 @@
+tea_varities = ["Black","Green","Oolong","White"]
+# print(tea_varities[1:3])
+# tea_varities[3]="Herbal"
+# print(tea_varities)
+
+# tea_varities[1:3] = ["Jasmine","Chamomile"]
+# print(tea_varities)
+
+# tea_varities[1:1]=["Oolong","White"]
+# print(tea_varities)
+
+# tea_varities.append("Earl Grey")
+# print(tea_varities)
+
+# print("pop: ",tea_varities.pop())
+# print("remove: ",tea_varities.remove("Oolong"))
+
+# tea_varities_copy = tea_varities.copy()
+# print(tea_varities_copy)
+
+# squared = [x**2 for x in range(10)]
+
+squared_nums = [x**2 for x in range(10)]
+print(squared_nums)
+
+cube_nums = [x**3 for x in range(10)]
+print(cube_nums)
