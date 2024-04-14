2dcb7fbf85671698b3c009db5c4aa6660cc1667a
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-04-14 15:45:00 +0530

    Type of errors in Python

---
+++ core_python/09_error_handling/error_handle.py
@@ -0,0 +1,45 @@
+file = open('youtube.txt', 'w')
+
+try:
+    file.write('chai aur code')
+finally:
+    file.close()
+
+with open('youtube.txt', 'w') as file:
+    file.write('chai aur python')
+    
+# Type of errors in Python
+# 1. Syntax Error
+# 2. Runtime Error
+# 3. Logical Error
+# 4. Exception Error
+# 5. Semantic Error
+# 6. Indentation Error
+# 7. Name Error
+# 8. Type Error
+# 9. Value Error
+# 10. Zero Division Error
+# 11. Attribute Error
+# 12. Key Error
+# 13. Index Error
+# 14. File Not Found Error
+# 15. Import Error
+# 16. Module Not Found Error
+# 17. Memory Error
+# 18. Recursion Error
+# 19. System Error
+# 20. Stop Iteration Error
+# 21. AssertionError
+# 22. NotImplementedError
+# 23. Unbound Local Error
+# 24. Overflow Error
+# 25. Floating Point Error
+# 26. Zero Division Error
+# 27. Arithmetic Error
+# 28. EOF Error
+# 29. Lookup Error
+# 30. Tab Error
+# 31. Unicode Error
+
+
+
