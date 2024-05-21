453d3c31a2f3209641ce98470d0f4683ca3ee852
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-05-21 06:29:53 +0000

    Jinja templates in Django

--- django/chaiaurdjango/chaiaurdjango/settings.py
+++ django/chaiaurdjango/chaiaurdjango/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chai',
     'tailwind',
     'theme',
     'django_browser_reload',
