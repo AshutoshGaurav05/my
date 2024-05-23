12fe5b47505a02de36eb32929daf11fb49db4de8
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-05-23 16:45:32 +0530

    image and its url in django

--- django/chaiaurdjango/chai/models.py
+++ django/chaiaurdjango/chai/models.py
@@ -14,6 +14,8 @@ class ChaiVariety(models.Model):
   image = models.ImageField(upload_to='chais/')
   date_added = models.DateTimeField(default=timezone.now)
   chai_type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES, default='ML')
+  price = models.DecimalField(default=0,max_digits=5, decimal_places=2)
+  description = models.TextField(default='')
 
   def __str__(self):
     return self.name
--- django/chaiaurdjango/chai/views.py
+++ django/chaiaurdjango/chai/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import ChaiVariety
 # Create your views here.
 def all_chai(request):
-    return render(request, 'chai/all_chai.html')
+    chai = ChaiVariety.objects.all()
+    return render(request, 'chai/all_chai.html', {'chais': chai})
+
+def chai_detail(requset,chai_id):
+    chai = get_object_or_404(ChaiVariety, pk=chai_id)
+    return render(requset, 'chai/chai_detail.html', { 'chai':chai})
